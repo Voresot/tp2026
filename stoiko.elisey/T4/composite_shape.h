598cfc3247d46a3c81f6b3bccb13aebb4430c33a
Elisey Stoiko <eliseystoiko@gmail.com>
2026-03-10 10:28:05 +0300

    Fix the rule of five

--- stoiko.elisey/T4/composite_shape.h
+++ stoiko.elisey/T4/composite_shape.h
@@ -11,6 +11,13 @@ class CompositeShape : public Shape {
 private:
     std::vector<std::unique_ptr<Shape>> shapes;
 public:
+    CompositeShape() = default;
+    CompositeShape(const CompositeShape& other) = delete;
+    CompositeShape(CompositeShape&& other) noexcept = default;
+    CompositeShape& operator=(const CompositeShape& other) = delete;
+    CompositeShape& operator=(CompositeShape&& other) noexcept = default;
+    ~CompositeShape() override = default;
+
     void addShape(std::unique_ptr<Shape> shape);
     double getArea() const override;
     Point getCenter() const override;
