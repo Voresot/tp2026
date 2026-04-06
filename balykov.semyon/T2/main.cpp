db3aaa8de9dee38d44912cb57def9f53014d407b
Balykov Semyon <balykov.semen2010@mail.ru>
2026-04-06 23:30:12 +0300

    Fix line endings globally

--- balykov.semyon/T2/main.cpp
+++ balykov.semyon/T2/main.cpp
@@ -46,16 +46,15 @@ private:
     std::basic_ios<char>::fmtflags fmt_;
 };
 std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
-std::istream& operator>>(std::istream& in,
-    LabelIO&& dest);
+std::istream& operator>>(std::istream& in, LabelIO&& dest);
 std::istream& operator>>(std::istream& in,
     ULLBinIO&& dest);
 std::istream& operator>>(std::istream& in,
     ComplexIO&& dest);
 std::istream& operator>>(std::istream& in, StringIO&& dest);
-std::istream& operator>>(std::istream& in, 
+std::istream& operator>>(std::istream& in,
     DataStruct& dest);
-std::ostream& operator<<(std::ostream& out, 
+std::ostream& operator<<(std::ostream& out,
     const DataStruct& dest);
 
 iofmtguard::iofmtguard(std::basic_ios<char>& s) :
