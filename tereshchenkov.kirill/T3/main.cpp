e971bbb5f48e6eb698bd5174ae53173381690a39
Ministrius <kter21146@ggmail.com>
2026-04-22 16:26:18 +0300

    Fix build2

--- tereshchenkov.kirill/T3/main.cpp
+++ tereshchenkov.kirill/T3/main.cpp
@@ -110,6 +110,7 @@ Polygon parsePolygon(const std::string& line) {
     return (poly.points.size() == static_cast<size_t>(n)) ? poly : Polygon{};
 }
 
+
 int main(int argc, char* argv[]) {
     if (argc < 2) return 1;
     std::ifstream file(argv[1]);
